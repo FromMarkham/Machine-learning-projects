5415009a2971d8b093a39ae4317f5c598b6c32a6
ElectroWang⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-08-26 17:34:20 -0400

    Create Word2vec ENHANCED EDITION.py

---
+++ Word2vec ENHANCED EDITION.py
@@ -0,0 +1,28 @@
+from gensim.models import Word2Vec
+
+Words=["The black-throated loon (Gavia arctica),", "also known as the Arctic loon and the black-throated diver,", "is a migratory aquatic bird found in the northern hemisphere,"]
+
+clean_data=[word.lower().split() for word in Words]
+
+model=Word2Vec(sentences=clean_data,vector_size=4,window=4,min_count=1,epochs=10,workers=2)
+
+
+
+def loop():
+    wordlist=[i.lower().split() for i in Words]
+    for t in wordlist:
+        b=model.wv[t]
+        return b
+        
+        #b=model.wv[i]
+        #return b
+
+word=model.wv["the"]
+
+
+
+print(word)
+loop()
+
+#https://radimrehurek.com/gensim/models/word2vec.html
+#https://medium.com/@manansuri/a-dummys-guide-to-word2vec-456444f3c673
