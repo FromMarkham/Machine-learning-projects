ee309bc927db3abe46695394407a50363a65c8f8
ElectroWang⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-08-30 20:40:40 -0400

    Update lstm.py

---
+++ lstm.py
@@ -0,0 +1,7 @@
+import tensorflow as tf 
+
+mylstm=tf.keras.layers.Sequential[tf.keras.Layers.lstm(),tf.keras.layers.dense()]
+mylstm.compile[]
+
+#https://www.tensorflow.org/api_docs/python/tf/keras/layers/LSTM
+#https://pieriantraining.com/tensorflow-lstm-example-a-beginners-guide/
