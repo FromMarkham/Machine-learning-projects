56b143bc234121abea47ab9df80db7f13577cdf8
ElectroWang⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-09-01 18:01:26 -0400

    Update I tried making a CNN off the back of my head.py
    AYYO IT WORKS👌👌👌👌👌👌👌👌👌

--- I tried making a CNN off the back of my head.py
+++ I tried making a CNN off the back of my head.py
@@ -12,18 +12,18 @@ class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
 
 #Conv2D.__init__() missing 2 required positional arguments: 'filters' and 'kernel_size'
 cnn_model=models.Sequential()
-cnn_model.add(layers.Conv2D(32,3,3),input_shape=(32,32,3),activation='relu')
+cnn_model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)))
 cnn_model.add(layers.MaxPool2D())
 cnn_model.add(layers.Conv2D(1,1))
 cnn_model.add(layers.MaxPool2D())
 
 cnn_model.add(layers.Flatten())
-cnn_model.add(layers.Dense(5))
-cnn_model.add(layers.Dense(5))
+cnn_model.add(layers.Dense(64, activation='relu'))
+cnn_model.add(layers.Dense(10))
 
-cnn_model.compile(optimizer='Lion',Loss='BinaryCrossentropy',Metrics=['Accuracy','BinaryAccuracy'])
+cnn_model.compile(optimizer='Lion',loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['Accuracy','BinaryAccuracy'])
 
-cnn_model.fit(train_images,train_labels,validation_data=(test_images,test_labels),epochs=10)
+train_cnn=cnn_model.fit(train_images,train_labels,epochs=10,validation_data=(test_images,test_labels))
 
 cnn_model.summary()
 
