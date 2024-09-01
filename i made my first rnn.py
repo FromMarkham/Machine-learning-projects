99029cd9f9418a1618d4c021d85d1407d1af4562
ElectroWang⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-09-01 19:33:52 -0400

    Update i made my first rnn.py

--- i made my first rnn.py
+++ i made my first rnn.py
@@ -3,9 +3,12 @@ import tensorflow as tf
 import keras
 from keras import layers
 
+import numpy as np 
+xtrain=np.ndarray([])
+ytrain=np.ndarray([]) 
 
 my_rnn=models.Sequential()
-my_rnn.add(keras.layers.rnn)
+my_rnn.add(tf.keras.layers.SimpleRNN(128))
 
 
 #https://www.tensorflow.org/guide/keras/working_with_rnns
