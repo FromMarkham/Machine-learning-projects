d404420c65fce4bdf47a28cd7511335441dcdc85
ElectroWang⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-08-31 22:06:13 -0400

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -1,13 +1,13 @@
 import tensorflow as tf 
 
-xtrain=
-ytrain=
-xtest=
-ytest=
+xtrain=[1,2,3,4,5]
+ytrain=['q','w','e','r','t']
+xtest=[]
+ytest=[]
 
 mylstm=tf.keras.layers.Sequential[tf.keras.Layers.lstm(),tf.keras.layers.dense()]
 mylstm.compile(optimizer='Lion',Loss='BinaryCrossentropy',Metrics=['Accuracy','BinaryAccuracy'])
 
-
+mylstm.fit(xtrain,ytrain,epochs=4)
 #https://www.tensorflow.org/api_docs/python/tf/keras/layers/LSTM
 #https://pieriantraining.com/tensorflow-lstm-example-a-beginners-guide/
