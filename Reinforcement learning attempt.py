7186f5b11c873b7cddaa6251baf203df3260dc2a
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-10-01 20:22:02 -0400

    Update Reinforcement learning attempt.py

--- Reinforcement learning attempt.py
+++ Reinforcement learning attempt.py
@@ -1,6 +1,16 @@
+#https://www.youtube.com/watch?v=cO5g5qLrLSo
 import gym 
 import tensorflow 
 import keras 
 import random
 
 environment=gym.make('CartPole-v1')
+
+states=environment.observation_space.shape[0]
+actions=environment.action_space.n
+
+states
+actions
+
+environment.reset()
+environment.render()
