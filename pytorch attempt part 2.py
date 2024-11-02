da6acf888ff0c63f5243e7f8e3eb9ea1a313aaac
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-02 16:57:19 -0400

    Update pytorch attempt part 2.py

--- pytorch attempt part 2.py
+++ pytorch attempt part 2.py
@@ -1,5 +1,6 @@
 
 class my_neural_network(nn.Module)
-  self.layer1
-  self.layer2
-  self.layer3
+  self.layer1=nn.Linear(100,100)
+  self.layer2=nn.relu(100,100)
+  self.layer3=nn.sigmoid(100,100)
+  self.layer4wtf=nn.Linear(100,100)
