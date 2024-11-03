733fe56cfcec9db30f31c5ba1a18bc20e18d3934
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-02 21:00:01 -0400

    Update pytorch attempt part 2.py

--- pytorch attempt part 2.py
+++ pytorch attempt part 2.py
@@ -14,15 +14,17 @@ dataloadertest=DataLoader(dataset=testing)
 class my_neural_network(nn.Module)
       def __init__(self):
           self.layer1=nn.Linear(100,100)
-          self.layer2=nn.relu(100,100)
-          self.layer3=nn.sigmoid(100,100)
-          self.layer4wtf=nn.Linear(100,100)
+          self.layer2=nn.ReLU()
+          self.layer3=nn.Linear(100,100)
+          self.layer4=nn.sigmoid()
+          self.layer5wutdahelllohmygawdddddddd=nn.Linear(100,100)
 
-      def my_neural_networkpart2():
+      def my_neural_networkpart2(x):
             output=self.layer1(x)
             output=self.layer2(output)
             output=self.layer3(output)
-            output=self.layer4wtf(output)
+            output=self.layer4(output)
+            output=self.layer5wutdahelllohmygawdddddddd(output)
             
             return output 
             
