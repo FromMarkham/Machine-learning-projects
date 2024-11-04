655a32c0a6ee474673d86bc5afa12865a31a9f8c
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-04 18:46:00 -0500

    Create Pytorch cnn.py

---
+++ Pytorch cnn.py
@@ -0,0 +1,6 @@
+#https://docs.kanaries.net/topics/Python/nn-linear#:~:text=The%20nn.Linear,is%20out_features.
+#https://stackoverflow.com/questions/55348647/weight-matrix-dimension-intuition-in-a-neural-network
+#https://www.youtube.com/watch?v=pDdP0TFzsoQ
+
+class my_cnn(nn.module):
+      self.my_cnn_layer=nn.Conv2d(4,4,4)
