01f0bb7b073b33a095b76c3800fa1c4321b6ce51
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-06 20:00:29 -0500

    Update my_little_lstm.py

--- my_little_lstm.py
+++ my_little_lstm.py
@@ -1,4 +1,12 @@
 #https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
 #https://www.youtube.com/watch?v=RHGiXPuo_pI
-da_numbers=torch.randn()
+
+
+da_numbers=torch.randn(19,13,10)
+
+da_numbersp2=torch.randn(19,13,10)
+
+da_numbersp3=torch.randn(19,13,10)
+
 my_little_lstm=nn.LSTM(10,10,10)
+out=my_little_lstm(da_numbers,(da_numbersp2,da_numbersp3))
