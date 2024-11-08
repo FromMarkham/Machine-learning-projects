b583c1a01658cbd9d400ae3987f83c1f815f2c96
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-07 21:26:12 -0500

    Update my_little_lstm.py

--- my_little_lstm.py
+++ my_little_lstm.py
@@ -1,5 +1,3 @@
-#https://www.youtube.com/watch?v=RHGiXPuo_pI
-
 import torch 
 import torch.nn as nn 
 import torch.nn.functional as F
@@ -8,3 +6,11 @@ from torch.optim.adam import Adam
 import lightning as L 
 from torch.utils.Data import TensorDataset, DataLoader 
 
+class my_little_lstm_aww(L.LightningModule):
+    def __init__(self):
+
+    def this_is_where_it_lives(self):
+
+    def optimise_lstm(self):
+
+    def train_network(self):
