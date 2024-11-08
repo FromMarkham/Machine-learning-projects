b270c37bbcab33b75305be76338af20a580910e1
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-07 21:21:15 -0500

    Update my_little_lstm.py

--- my_little_lstm.py
+++ my_little_lstm.py
@@ -1,13 +1,10 @@
-#https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
 #https://www.youtube.com/watch?v=RHGiXPuo_pI
 
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F
+from torch.optim.adam import Adam 
 
-da_numbers=torch.randn(19,13,10)
+import lightning as L 
+from torch.utils.Data import TensorDataset, DataLoader 
 
-da_numbersp2=torch.randn(19,13,10)
-
-da_numbersp3=torch.randn(19,13,10)
-
-my_little_lstm=nn.LSTM(10,10,10)
-
-out=my_little_lstm(da_numbers,(da_numbersp2,da_numbersp3))
