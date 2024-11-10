9299c2dee1e9b862df71004c9f139b9d1db10aae
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-09 22:25:15 -0500

    Update my_little_lstm.py

--- my_little_lstm.py
+++ my_little_lstm.py
@@ -7,10 +7,16 @@ import lightning as L
 from torch.utils.Data import TensorDataset, DataLoader 
 
 class my_little_lstm_aww(L.LightningModule):
+    mean=torch.tensor(0.5)
+    standarddeviation=torch.tensor(1.0)
+    
     def __init__(self):
-
+        self.weight=nn.Parameter()
+        
     def this_is_where_it_lives(self):
 
     def optimise_lstm(self):
 
     def train_network(self):
+        
+    
