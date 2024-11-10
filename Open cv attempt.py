1d4660a54cfaa2efa7a0b832ea437424b6c50649
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-09 21:49:36 -0500

    Update Open cv attempt.py

--- Open cv attempt.py
+++ Open cv attempt.py
@@ -7,8 +7,13 @@ import cv2
 
 my_image=cv2.imread('/Users/bobwang/Downloads/33715.png')
 
-my_video=cv2.VideoCapture(1)
+#my_video=cv2.VideoCapture(1)
 
 #cv2.imshow('Robot',my_image)
 
 #cv2.waitKey(100000000)
+#https://youtu.be/oXlwWbU8l2o?t=587
+y_video=cv2.VideoCapture('//Users/bobwang/Downloads/40%20Minute%20Timer.mp4')
+
+while True:
+    isTrue,frame=y_video.read()
