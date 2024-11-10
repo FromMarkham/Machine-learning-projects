27377d08ef3dc51b908ed6d98ba1c4d316b06c11
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-09 21:36:15 -0500

    Update Open cv attempt.py

--- Open cv attempt.py
+++ Open cv attempt.py
@@ -1,3 +1,12 @@
-import cv2 as cv 
+#https://www.geeksforgeeks.org/python-opencv-waitkey-function/
+import cv2  
 
-video=cv.VideoCapture()
+#video=cv.VideoCapture()
+
+#https://www.youtube.com/watch?v=oXlwWbU8l2o&t=627s
+
+my_image=cv2.imread('/Users/bobwang/Downloads/33715.png')
+
+cv2.imshow('Robot',my_image)
+
+cv2.waitKey(100000000)
