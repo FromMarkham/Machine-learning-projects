f5cbfcc4f996f7e3c0e4e2cb973b8da674606a70
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-13 21:31:17 -0500

    Update my_little_lstm.py

--- my_little_lstm.py
+++ my_little_lstm.py
@@ -1,4 +1,5 @@
 import torch 
+import torch 
 import torch.nn as nn 
 import torch.nn.functional as F
 from torch.optim.adam import Adam 
@@ -7,8 +8,11 @@ import lightning as L
 from torch.utils.Data import TensorDataset, DataLoader 
 #https://www.youtube.com/@statquest/videos
 #https://pytorch.org/docs/stable/generated/torch.nn.Embedding.html
+#https://pytorch.org/tutorials/beginner/nlp/word_embeddings_tutorial.html
 
-my_embeddings=nn.Embedding()
+my_words_lmao={}
+
+my_embeddings=nn.Embedding(2,5)
 
 class my_little_lstm_aww(L.LightningModule):
     mean=torch.tensor(0.5)
@@ -18,10 +22,22 @@ class my_little_lstm_aww(L.LightningModule):
 
         super().__init__()
         
-        self.weight=nn.Parameter(torch.normal(),requires_grad=True)
-        self.weight2=nn.Parameter(torch.normal(),requires_grad=True)
-        self.bias=
-    
+        self.weight=nn.Parameter(torch.normal(mean=mean,std=standarddeviation),requires_grad=True)
+        self.weight2=nn.Parameter(torch.normal(mean=mean,std=standarddeviation),requires_grad=True)
+        self.bias=nn.Parameter(torch.tensor(0),requires_grad=True)
+
+        self.weigh3=nn.Parameter(torch.normal(mean=mean,std=standarddeviation),requires_grad=True)
+        self.weight4=nn.Parameter(torch.normal(mean=mean,std=standarddeviation),requires_grad=True)
+        self.bias1=nn.Parameter(torch.tensor(0),requires_grad=True)
+
+        self.weight5=nn.Parameter(torch.normal(mean=mean,std=standarddeviation),requires_grad=True)
+        self.weight6=nn.Parameter(torch.normal(mean=mean,std=standarddeviation),requires_grad=True)
+        self.bias2=nn.Parameter(torch.tensor(0),requires_grad=True)
+
+        self.weight7=nn.Parameter(torch.normal(mean=mean,std=standarddeviation),requires_grad=True)
+        self.weight8=nn.Parameter(torch.normal(mean=mean,std=standarddeviation),requires_grad=True)
+        self.bias3=nn.Parameter(torch.tensor(0),requires_grad=True)
+        
     def this_is_where_it_lives(self):
 
     def optimise_lstm(self):
