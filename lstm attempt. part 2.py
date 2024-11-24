aaf8df8bc5ead13b74d22e0bf3a77a5b563c6d0f
SciTechBoi⚡️💻🧑‍🔬 <bwang@markhamfirebirds.ca>
2024-11-23 21:29:13 -0500

    Update lstm attempt. part 2.py

--- lstm attempt. part 2.py
+++ lstm attempt. part 2.py
@@ -1,6 +1,11 @@
 
 
 
-class my_little_lstm(self) 
-    def __init__()
-        self.weight1=nn.Parameter(mean=0.5,std=1)
+class my_little_lstm(L.LightningModule):
+    
+    #initialise them glorious parameters
+    def __init__():
+        self.weight1=nn.Parameter(torch.normal(mean=0.5,std=1),requires_grad=true)
+        self.bias1=nn.Parameter(torch.tensor[0],requires_grad=true)
+        self.weight2=nn.Parameter(torch.normal(mean=0.5,std=1),requires_grad=true)
+        self.bias=nn.Parameter(torch.tensor[0],requires_grad=true)
